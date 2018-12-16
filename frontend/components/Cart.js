7a9c7b95f698c961f23ba04e6aa8801ba25bd0dc
Jeremy Winterberg <jeremy@jeremydwayne.com>
2018-12-16 05:37:29 -0600

    nested render props moved to composed react-adopt

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Query, Mutation} from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { adopt } from 'react-adopt';
 import CartStyle from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
@@ -22,34 +23,37 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `;
 
+// Cleans up the render prop nested mess
+const Composed = adopt({
+  user: ({render} ) =>  <User>{render}</User>,
+  toggleCart: ({render} ) =>  <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
+  localState: ({render} ) =>  <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
+});
+
 const Cart = () => (
-  <User>{({data: {me}}) => {
-    if(!me) return null;
-    return (
-      <Mutation mutation={TOGGLE_CART_MUTATION}>
-        {(toggleCart) => (
-          <Query query={LOCAL_STATE_QUERY}>
-            {({ data }) => (
-              <CartStyle open={data.cartOpen}>
-                <header>
-                  <CloseButton onClick={toggleCart} title="close">&times;</CloseButton>
-                  <Supreme>{me.name}'s Cart</Supreme>
-                  <p>You have {me.cart.length} item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
-                </header>
-                <ul>
-                  {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}
-                </ul>
-                <footer>
-                  <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                  <SickButton>Checkout</SickButton>
-                </footer>
-              </CartStyle>
-            )}
-          </Query>
-        )}
-      </Mutation>
-  )}}</User>
+  <Composed>
+    {({ user, toggleCart, localState }) => {
+      const me = user.data.me;
+      if (!me) return null;
+      return (
+        <CartStyle open={localState.data.cartOpen}>
+          <header>
+            <CloseButton onClick={toggleCart} title="close">&times;</CloseButton>
+            <Supreme>{me.name}'s Cart</Supreme>
+            <p>You have {me.cart.length} item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
+          </header>
+          <ul>
+            {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}
+          </ul>
+          <footer>
+            <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+            <SickButton>Checkout</SickButton>
+          </footer>
+        </CartStyle>
+      );
+    }}
+  </Composed>
 );
 
 export default Cart;
-export {LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION};
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
